848207fc513ca1e8bee1b0820fd0a74cae526352
Martin <martin.marochkin@gmail.com>
2023-10-08 22:11:59 +0300

    отскок от стенок

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ root.geometry("800x600")
  
 x = 375
 y = 500 
-m = -10
+mx = -10
+my = -10
 
 canvas = Canvas(bg="black", width=800, height=600)
 canvas.pack(anchor=CENTER, expand=1)
@@ -14,14 +15,16 @@ ov = canvas.create_oval(x, y, x + 50, y + 50, width=2, outline="red")
 
 def main():
     ball()
-    root.after(50, main)
+    root.after(20, main)
 
 def ball():
-    global m
-    canvas.move(ov, 0, m)
+    global mx, my
+    canvas.move(ov, mx, my)
     x1, y1, x2, y2 = canvas.coords(ov)
-    if (m < 0 and y1 <= 0) or (m > 0 and y2 >= 600):
-        m = m * -1
+    if (mx < 0 and x1 <= 0) or (mx > 0 and x2 >= 800):
+        mx = mx * -1
+    if (my < 0 and y1 <= 0) or (my > 0 and y2 >= 600):
+        my = my * -1    
         
 
 main()
